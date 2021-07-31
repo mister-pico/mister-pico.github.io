8cbd0b0f2bf5645b43874a16cf3843c0ee56803d
mister-pico <88174928+mister-pico@users.noreply.github.com>
2021-07-31 14:17:07 -0700

    Update script.js
    increased frames per second

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function myMove() {
   const elem = document.getElementById("animate");
   let pos = 0;
   clearInterval(id);
-  id = setInterval(frame, 5);
+  id = setInterval(frame, 10);
 
   function frame() {
     if (pos == 670) {
